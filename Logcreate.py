70eb625e77fc4ce75b06d60dcb6ecaf151c3972a
kai Nishio <kairi240@icloud.com>
2025-09-08 23:52:42 +0900

    ネットワーク切断のコードを進めた

--- Logcreate.py
+++ Logcreate.py
@@ -7,29 +7,51 @@ bucket ="rescorr"
 
 #CPU高負荷
 def CPUtra(core=None,donetime=5):
-    basiccore =1
+    basiccore =1#コア数1
     if core is None:
         core = os.core() or basiccore
     createfile = f"CPUtraffic{int(time.time())}.log" #ファイル名被り対策にtime使ってる
     try:
         with open(createfile, "w") as wfile:
             wfile.write("start,core:{core},donetime:{donetime}.\n")
-            subprocess.run(["stress","--cpu",str(core),"--timeout",str(donetime)])
+            command =subprocess.run(["stress","--cpu",str(core),"--timeout",str(donetime)])
             print(f"Executing command: {' '.join(command)}")
             subprocess.run(command, check=True)
             wfile.write("finish.\n")
-    except:
+    except FileNotFoundError:
+        print("nofile")
+        return None
+    except subprocess.CalledProcessError:
+        print("cannt do command")
+        return None
     return createfile
 
 #ネットワーク切断
-def Netkill():
+def Netkill(interface="enX0",waittime=5):
     createfile = f"Networkkill{int(time.time())}.log" #ファイル名被り対策にtime使ってる
-    waittime=5
-    with open(createfile,"w") as wfile:
-        wfile.write("start")
-        subprocess.run(["sudo", "ip", "link", "set", "eth0", "down"], wfile, wfile)
-        time.sleep(waittime)
-        subprocess.run(["sudo", "ip", "link", "set", "eth0", "up"], wfile, wfile)
+    down =["sudo", "ip", "link", "set", interface, "down"]
+    up=["sudo", "ip", "link", "set", interface, "up"]
+    try:
+        with open(createfile,"w") as wfile:
+            wfile.write("start")
+            wfile.flush()
+            subprocess.run(down, stdout=wfile, stderr=wfile)
+            time.sleep(waittime)
+
+            subprocess.run(["sudo", "ip", "link", "set", "enX0", "up"], stdout=wfile, stderr=wfile)
+            
+    except FileNotFoundError:
+            print("nofile")
+            return None
+    except subprocess.CalledProcessError:
+        print("cannt do command")
+    except KeyboardInterrupt:.
+        print("canceled")
+    finally:
+        try:
+        except Exception:
+            return None
+    with open(createfile,"a"):
         wfile.write("finish")
     return createfile
 #プロセスクラッシュ(疑似)
@@ -40,3 +62,8 @@ def Processcra():
         subprocess.run(["kill", "-9", "99999"], wfile, wfile)
         wfile.write("finish")
     return createfile
+
+if __name__ =="__main__":
+    netinterface ="enX0"
+    Netkill(netinterface,10)
+
