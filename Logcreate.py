a743cb26e177a541a57b9c354a8d6f967e70b485
kai Nishio <kairi240@icloud.com>
2025-09-16 23:46:04 +0900

    stress_cpuの書き込む部分を書いてる

--- Logcreate.py
+++ Logcreate.py
@@ -5,32 +5,23 @@ import os
 import signal
 from botocore.exceptions import NoCredentialsError, BotoCoreError 
 import psutil#pip install pstil
+import threading
 
 #CPU高負荷
 def stress_cpu(core=None,donetime=5):
     """
     stressコマンドを使い、指定されたコア数と時間でCPUに負荷をかける。
-    要件: 'stress' コマンドがインストールされていること。
     """
-    basiccore =1#コア数1
-    if core is None:
-        core = os.cpu_count() or basiccore
-    createfile = f"stress_cpu{int(time.time())}.log" #ファイル名被り対策にtime使ってる
-    try:
-        with open(createfile, "w") as wfile:
-            wfile.write(f"start,core:{core},donetime:{donetime}.\n")
-            command =["stress","--cpu",str(core),"--timeout",str(donetime)]
-            print(f"Executing command: {' '.join(command)}")
-            subprocess.run(command, check=True)
-            wfile.write("finish.\n")
+    try:         
+        command =["stress","--cpu",str(core),"--timeout",str(donetime)]
+        print(f"Executing command: {' '.join(command)}")
+        subprocess.run(command, check=True)
     except FileNotFoundError:
         print("nofile")
         return None
     except subprocess.CalledProcessError:
         print("cannt do command")
         return None
-    return createfile
-
 #ネットワーク切断
 def disrupt_network(interface="enX0",waittime=5):
     """
@@ -101,23 +92,63 @@ def crash_process(process_script ="process.py",monitoringtime =30):
         with open(createfile, "a") as wfile:
             wfile.write("finish\n")
     return createfile
+#データ記録クラス   
+class MetricsCollector(threading.Thread):
+    def __init__(self,interval=1.0):
+        super().__init__()
+        self.interval=interval
+        self.correct_log = []
+        self._stop_event =threading.Event()
+    def run(self):
+        print("start")
+        while not self._stop_event.is_set():
+            timestanp = time.time()
+            cpu_percent = psutil.cpu_percent
+            memory_percent = psutil.virtual_memory().percent
+
+            self.correct_log({
+                "timestanp":timestanp,
+                "cpu_percent":cpu_percent,
+                "memory_percent":memory_percent,
+            })
+            time.sleep(self.interval)
+    def stop(self):
+        self._stop_event.set()
 if __name__ =="__main__":
-    netinterface ="enX0"
+    net_interface ="enX0"
 try:
     s3operate=sdk.client("s3")
     bucket ="rescorr"
+    collector = MetricsCollector(interval=1.0)
+
+    #stress_cpuの実行部分
+    collector.start()
+    createfile = f"stress_cpu{int(time.time())}.log" #ファイル名被り対策にtime使ってる
+    with open(createfile, "w") as wfile:
+        basiccore =1#コア数1
+        core =1#コア数ここで指定
+        donetime=10
+        if core is None:
+            core = os.cpu_count() or basiccore
+        wfile.write(f"start,core:{core},donetime:{donetime}.\n")
+        stress_cpu(core,donetime)
+    
+        collector.stop()
+        collector.join()
+        print(f"Collected data:{len(collector.correct_log)}")
+        for x in collector.correct_log:
+            print(x)
+            wfile.write(f"{x}\n")
+        wfile.write("finish.\n")
+
     logfile =crash_process(process_script="process.py",monitoringtime=10)
 except NoCredentialsError:
     print("AWS account not found")
     exit(1)
-class MetricsCollector:
-    def cpu():
-        
-
-if logfile:
+if createfile:
     try:
-        s3operate.upload_file(Filename = logfile,Bucket=bucket,Key =logfile)
-        os.remove(logfile)
+        s3operate.upload_file(Filename = createfile,Bucket=bucket,Key =createfile)
+        os.remove(createfile)
     except FileNotFoundError:
         print("dont found upload file")
     except BotoCoreError:
