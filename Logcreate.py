67e28a3b628a7866eb4d2b6c5f458a81bc362795
kai Nishio <kairi240@icloud.com>
2025-09-27 23:31:18 +0900

    コード完成

--- Logcreate.py
+++ Logcreate.py
@@ -7,18 +7,17 @@ from botocore.exceptions import NoCredentialsError, BotoCoreError
 import psutil#pip install pstil
 import threading
 from datetime import datetime
-import zoneinfo
 import csv
 
 #CPU高負荷
-def stress_cpu(core=None,donetime=5):
+def stress_cpu(core=None,duration=5):
     """
     stressコマンドを使い、指定されたコア数と時間でCPUに負荷をかける。
     """     
-    command =["stress","--cpu",str(core),"--timeout",str(donetime)]
+    command =["stress","--cpu",str(core),"--timeout",str(duration)]
     subprocess.run(command, check=True,capture_output=True)
 #ネットワーク切断
-def disrupt_network(interface="enX0",waittime=5):
+def disrupt_network(interface="enX0",duration=5):
     """
     指定されたネットワークインターフェースを一度ダウンさせ、指定時間後にアップさせる。
     注意: この関数は 'sudo' を必要とする。パスワードなしで 'ip' コマンドが実行できるか確認。
@@ -27,30 +26,29 @@ def disrupt_network(interface="enX0",waittime=5):
     up=["sudo", "ip", "link", "set", interface, "up"]
     try:
         subprocess.run(down, check=True, capture_output=True)
-        time.sleep(waittime)      
+        time.sleep(duration)
+    except subprocess.CalledProcessError:
+         print("cannt do command")
+    except KeyboardInterrupt:
+        print("canceled")      
     finally:
-        subprocess.run(up,check=True, capture_output=True,text=True)#デバック用detail
+        subprocess.run(up,check=True, capture_output=True)
 #プロセスクラッシュ
-def crash_process(process_script ="process.py",monitoringtime =30):
+def crash_process(process_script ="process.py",duration =30):
     """
     指定されたPythonスクリプトをサブプロセスとして起動し、一定時間後にSIGKILLで強制終了させる。
     """  
-    pid=None
+    process=None
     try:      
-            
-        
-        PROCESS = subprocess.Popen(["python","-u",process_script])
-        pid =PROCESS.pid
-        time.sleep(monitoringtime)
-    except KeyboardInterrupt:
-        print("process canceled")
+        process = subprocess.Popen(["python","-u",process_script])
+        time.sleep(duration)
     finally:
-        if pid:
-            print("ready crash")
-            os.kill(pid,signal.SIGKILL)
-            print("kill success")
+        if process and process.poll() is None:
+            os.kill(process.pid,signal.SIGKILL)
+        elif process:
+            print("  - Process had already terminated.")
         else:
-            print("PID is not start")
+            print("process is not started")
 #データ記録クラス   
 class MetricsCollector(threading.Thread):
     def __init__(self,interval=1.0):
@@ -63,8 +61,7 @@ class MetricsCollector(threading.Thread):
     def run(self):
         print("start")
         while not self._stop_event.is_set():
-            japan = zoneinfo.ZoneInfo("Asia/Tokyo")
-            timestanp = datetime.now(japan).isoformat() 
+            timestanp = datetime.datetime.now(datetime.timezone.utc).isoformat()
             #cpuメトリクス
             cpu_percent = psutil.cpu_percent()
             cpu_times = psutil.cpu_times_percent()#cpu使用時間
@@ -95,10 +92,11 @@ class MetricsCollector(threading.Thread):
                 "cpu_times_system":cpu_times.system,
                 "cpu_times_idle":cpu_times.idle,
                 "cpu_times_iowait":cpu_times.iowait,#ローカルじゃテストできない。Linaxでしか実行できないから
-                "load_avg":load_avg[0],
+                "load_avg_1m":load_avg[0],#1分
+                "load_avg_5m":load_avg[1],#5分
+                "load_avg_15m":load_avg[2],#15分
                 "memory_percent":memory_percent,
                 "swap_percent":swap_percent,
-                "current_disk_io":current_disk_io,
                 "disk_read_rate":disk_read_rate,
                 "disk_write_rate":disk_write_rate,
                 "net_sent_rate":net_sent_rate,
@@ -135,8 +133,8 @@ def run_experiment(func,args,type,local_dir):
     except Exception as e:
         print(f"error{e}")
     finally:
-        collector.join()
         collector.stop()
+        collector.join()
     file_path =save_to_csv(collector.correct_log,csv_filename,local_dir)
     return file_path
 
@@ -162,7 +160,7 @@ if __name__ =="__main__":
 
         network_log_filepath = run_experiment(
             func =disrupt_network,
-            args ={'interface':net_interface,'waittime':5},
+            args ={'interface':net_interface,'duration':5},
             type = 'disrupt_network',
             local_dir = LOCAL_LOG_DIR
         )
@@ -174,7 +172,7 @@ if __name__ =="__main__":
 
         process_log_filepath = run_experiment(
             func=crash_process,
-            args={'process_script':"process.py",'monitoringtime': 30},
+            args={'process_script':"process.py",'duration': 30},
             type='crash_process',
             local_dir=LOCAL_LOG_DIR
         )
